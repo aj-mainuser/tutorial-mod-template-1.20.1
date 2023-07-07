a1e6b8c904950912495ec91e5e4e2e940c8c701f
ashleyjoseph021 <54641995+ashleyjoseph021@users.noreply.github.com>
2023-07-07 13:11:52 -0600

    Initial commit

---
+++ src/main/java/aj/tutorial_mod/TutorialMod.java
@@ -0,0 +1,17 @@
+package aj.tutorial_mod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final	String MOD_ID = "tutorial_mod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
