f336257d6fd73c5082ec5becb1ffa1d50b5e731e
ashleyjoseph021 <54641995+ashleyjoseph021@users.noreply.github.com>
2023-07-14 13:39:48 -0600

    removed slab from item group
    Signed-off-by: ashleyjoseph021 <54641995+ashleyjoseph021@users.noreply.github.com>

--- src/main/java/aj/tutorial_mod/item/ModItemGroups.java
+++ src/main/java/aj/tutorial_mod/item/ModItemGroups.java
@@ -23,7 +23,6 @@ public class ModItemGroups {
 
                         entries.add(ModBlocks.RUBY_BLOCK);
                         entries.add(ModBlocks.RAW_RUBY_BLOCK);
-                        entries.add(ModBlocks.RUBY_SLAB);
 
             }).build());
 
